c7c45bdd97776f140726b03f4df218ddcaa9044d
fujohnwang <fujohnwang@gmail.com>
2012-08-10 10:06:42 +0800

    code clean up by passing state via sbt Command State

--- src/main/scala/com/github/fujohnwang/jvmole/commands/Commands.scala
+++ src/main/scala/com/github/fujohnwang/jvmole/commands/Commands.scala
@@ -1,25 +1,25 @@
 package com.github.fujohnwang.jvmole.commands
 
-import sbt.Command
+import sbt.{AttributeKey, Command}
 import sbt.complete.DefaultParsers._
 import scala.Some
-import com.github.fujohnwang.jvmole.JVMoleHeadQuarter
+import com.github.fujohnwang.jvmole.{ProjectInfo}
 import com.sun.tools.attach.VirtualMachine
 import management.ManagementFactory
 import collection.JavaConversions._
+import com.github.fujohnwang.jvmole.jmx.LocalJMXConnector
 
-trait Commands {
-  val CONNECTOR_ADDRESS = "com.sun.management.jmxremote.localConnectorAddress"
+trait Commands extends ProjectInfo with LocalJMXConnector {
+  val VIRTUAL_MACHINE_K = AttributeKey[VirtualMachine]("virtual machine")
 
-  def welcome = Command.command("welcome")(state => {
-    state.log.info("welcome to jvmole's world~")
-    state
-  })
+  def welcome = Command.command("welcome") {
+    state => state.log.info("welcome to jvmole's world~"); state
+  }
 
   /**
    * current JVMole process's pid will not be listed.
    */
-  def listVirtualMachines = Command.command("vms", "list running virtual machine instances that we can attach to", "")(s => {
+  def listVirtualMachines = Command.command("lsvm", "list running virtual machine instances that we can attach to", "")(s => {
     s.log.info("JVM processes where are running: ")
     val processName = ManagementFactory.getRuntimeMXBean.getName
     VirtualMachine.list().foreach(vm => if (!processName.startsWith(vm.id())) s.log.info("  " + vm.toString))
@@ -27,16 +27,17 @@ trait Commands {
   })
 
   def attach = Command("attach", ("attach <pid>", "attach to specific virtual machine instance as per pid"), "")(_ => token(Space) ~> token(Digit.+.map(_.mkString), "<pid>"))((s, pid) => {
-    JVMoleHeadQuarter.virtualMachine = Some(VirtualMachine.attach(pid))
-    s
+    val ns = s.put(VIRTUAL_MACHINE_K, VirtualMachine.attach(pid))
+    s.log.info("attach to JVM instance with pid=" + pid + " successfully.")
+    ns.addExitHook(() => detach)
   })
 
   def detach = Command.command("detach", "detach from attached JVM instance if any", "")(s => {
-    JVMoleHeadQuarter.virtualMachine match {
+    s.get(VIRTUAL_MACHINE_K) match {
       case None => s.log.warn("no attached JVM instance found when trying to detach it.")
-      case Some(vm) => vm.detach(); JVMoleHeadQuarter.virtualMachine = None
+      case Some(vm) => s.log.info("detach JVM instance."); vm.detach()
     }
-    s
+    s.remove(VIRTUAL_MACHINE_K)
   })
 
   def listMBeans = Command.command("beans", "list mbeans registered", "")(s => s)
@@ -46,7 +47,6 @@ trait Commands {
   val beanDesc = Command.command("desc", "describe mbean", "")(s => s)
 
   def execMBeanMethod = Command.command("exec", "invoke mbean method", "")(s => s)
-
 
 
 }
